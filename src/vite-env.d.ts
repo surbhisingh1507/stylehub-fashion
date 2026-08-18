4a5de1f4215367d98f6b77ee47c52ea4af8e9e57
Surbhi Singh <surbhi2626singh@gmail.com>
2026-08-18 10:29:33 +0530

    solve build issue

---
+++ src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+declare module "*.css?url" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
